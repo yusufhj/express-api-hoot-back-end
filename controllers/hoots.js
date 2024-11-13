a8a2b9b19eafde0960266363faa3bcf182fbfb0e
Yusuf AlHamad <ylive71@gmail.com>
2024-11-13 10:54:07 +0300

    deleteee hoote

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -70,5 +70,22 @@ router.put('/:hootId', async (req, res) => {
     }
 });
 
+// DELETE /hoots/:hootId
+router.delete('/:hootId', async (req, res) => {
+    try {
+        const hoot = await Hoot.findById(req.params.hootId);
+
+        if (!hoot.author.equals(req.user._id)) {
+            return res.status(403).send("You're not allowed to do that!")
+        }
+        
+        const deletedHoot = await Hoot.findByIdAndDelete(req.params.hootId);
+        
+        res.status(200).json(deletedHoot);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+});
+
 
 module.exports = router;
