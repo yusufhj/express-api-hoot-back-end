1d47c3e1ebfbb21f0893a94e4cb85451d0a7ba19
Yusuf AlHamad <ylive71@gmail.com>
2024-11-13 09:21:07 +0300

    init commit

---
+++ server.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+require('./config/database');
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const verifyToken = require('./middleware/verify-token');
+
+// Controllers
+const testJwtCtrl = require('./controllers/test-jwt');
+const usersRouter = require('./controllers/users');
+const profilesRouter = require('./controllers/profiles');
+
+const app = express();
+app.use(express.json());
+app.use(morgan('dev'));
+app.use(cors());
+
+
+// Public Routes
+app.use('/test-jwt', testJwtCtrl);
+app.use('/users', usersRouter);
+
+// Private Routes
+app.use(verifyToken);
+app.use('/profiles', profilesRouter);
+
+app.listen(3000, () => {
+  console.log('The express app is ready!');
+});
